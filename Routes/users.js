b2cec025ff09282283e33cb38c127df6509f7dbd
Nadia <nlnadialigia@gmail.com>
2021-02-14 11:38:49 -0300

    feat: add user routes authentication

--- Routes/users.js
+++ Routes/users.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import express from 'express';
 import Users from '../model/user.js';
 const router = express.Router();
@@ -26,6 +27,24 @@ router.post('/create', (request, response) => {
       return response.send(data);
     });
   });
+});
+
+router.post('/auth', (request, response) => {
+  const { email, password } = request.body;
+
+  if (!email || !password) return response.send('Dados insuficientes para autenticação do usuário');
+
+  Users.findOne({ email }, (error, data) => {
+    if (error) return response.send({ error: 'Erro ao buscar usuário!' });
+    if (!data) return response.send({ error: 'Usuário não registrado' });
+
+    bcrypt.compare(password, data.password, (_, same) => {
+      if (!same) return response.send({ error: 'Erro ao autenticar o usuário' });
+
+      data.password = undefined;
+      return response.send(data);
+    });
+  }).select('+password');
 });
 
 export default router;
