79afc75587a706892de405ab8474ec2498660568
Nadia <nlnadialigia@gmail.com>
2021-02-14 02:36:22 -0300

    chore: add create users route

--- Routes/users.js
+++ Routes/users.js
@@ -1,12 +1,16 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/users', (request, response) => {
+router.get('/', (request, response) => {
   return response.send({ message: 'Tudo ok com o método GET da rota users' });
 });
 
-router.post('/users', (request, response) => {
+router.post('/', (request, response) => {
   return response.send({ message: 'Tudo ok com o método POST da rota users' });
+});
+
+router.post('/create', (request, response) => {
+  return response.send({ message: 'Seu usuário foi criado' });
 });
 
 export default router;
