f521862c79571a34eca46ff549385973a8357fea
Nadia <nlnadialigia@gmail.com>
2021-02-14 03:07:46 -0300

    feat: add Mongodb conection

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 import express from 'express';
 import indexRoute from './Routes/index.js';
 import usersRoute from './Routes/users.js';
+import mongoose from 'mongoose';
+// import bodyParser from 'body-parser';
+import { config } from 'dotenv';
+
+config();
+
+mongoose.connect(
+  'db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+);
+
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', () => {
+  console.log('Erro na conexão com o banco de dados');
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Aplicação desconectada do banco de dados');
+});
 
 const app = express();
 
